5f0289ac0f78554e2ec24866584a7a33d8400327
Tharindu Deshan <109593849+atdeshan@users.noreply.github.com>
2023-07-31 22:58:05 +0530

    change Quiz

--- CAssignmentFinal/main.c
+++ CAssignmentFinal/main.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
 
 struct Quiz {
     char question[1000];
-    char answer[50]; // Increased the size to 50 to allow longer answers
+    char answer[50];
 };
-
+//initilize the quiz and answers
 void initialize(struct Quiz quiz[], int* numberOfQuestions);
+//save those
 void tosaveFile(struct Quiz quiz[], int* numberOfQuestions);
+//load the struct in to text.txt
 void loadFile(struct Quiz quiz[], int* numberOfQuestions);
+
+    
+
 
 int main() {
     int numberOfQuestions = 0;
     struct Quiz quiz[100];
     int userNumber;
 //
-    printf("01 - Change max count of quizes\n02 - Face for the questoins\n03 - Rearrange the quizes\n");
+    printf("1 - Face for the questoins\n2 - Rearrange the quizes\n");
     printf("Enter a number - ");
     scanf("%d",&userNumber);
     printf("\n\n------------------------------------------------\n\n");
@@ -24,11 +32,8 @@ int main() {
     char isZpressed[2];
     int n=0;
     switch(userNumber){
-        case 1 :
-            printf("Enter tha maximum number of quizes number that may ask\nEnter the number - ");
-            scanf("%d",&numberOfQuestions);
-            break;
-        case 2:
+        
+        case 1:
             printf("for each answers use YES or NO\n");
             loadFile(quiz, &numberOfQuestions);
             char answerFormUser[50];
@@ -36,9 +41,9 @@ int main() {
                 printf("%s\n",quiz[n].question);
                 scanf("%s",answerFormUser);
                 if(strcmp(answerFormUser,quiz[n].answer) ==0 ){
-                    printf("/nYour answer is correct\n");
+                    printf("Your answer is correct\n");
                 }else{
-                    printf("\nyour answer is wrong");
+                    printf("your answer is wrong\n");
                 }
                 printf("\nEnter do you willing end the quiz therwise enter a any number to next question\n");
                 scanf("%s",isZpressed);
@@ -46,12 +51,12 @@ int main() {
             }while((strcmp(isZpressed,"Z")!=0) && (n<numberOfQuestions));
             n=0;
             break;
-        case 3:
+        case 2:
             initialize(quiz, &numberOfQuestions);
             tosaveFile(quiz, &numberOfQuestions);
             loadFile(quiz, &numberOfQuestions);
             break;
-            
+        
     }
 
 //    initialize(quiz, &numberOfQuestions);
@@ -70,14 +75,14 @@ void initialize(struct Quiz quiz[], int* numberOfQuestions) {
 
    
 
-    printf("\nEnter Problems and Answers\n\n");
+    printf("\nEnter Problems and Answers\n");
     for (int x = 0; x < (*numberOfQuestions); x++) {
         printf("Enter A Question: ");
 //        scanf("%[^\n]", quiz[x].question);
         fgets(quiz[x].question, sizeof(quiz[x].question), stdin);
        
 
-        printf("Enter the Answer: ");
+        printf("Enter the Answer(lowercase): ");
 //        scanf("%[^\n]", quiz[x].answer);
         fgets(quiz[x].answer, sizeof(quiz[x].answer), stdin);
 
@@ -114,7 +119,3 @@ void loadFile(struct Quiz quiz[], int* numberOfQuestions) {
     *numberOfQuestions = i;
     fclose(file);
 }
-
-
-
-
